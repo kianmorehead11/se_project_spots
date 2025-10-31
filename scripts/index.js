9d08213bb7b9b893bbf90997f2b1cf69105d6bce
Kianmorehead11 <kianmorehead11@gmail.com>
2025-10-30 17:26:06 -0700

    Spots stage 4 part 3 revision

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
@@ -8,52 +16,55 @@ const editProfileNameInput = editProfileModal.querySelector(
 const editProfileDescriptionInput = editProfileModal.querySelector(
   "#profile-description-input"
 );
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+editProfileBtn.addEventListener("click", () => {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+  openModal(editProfileModal);
+});
+
+editProfileCloseBtn.addEventListener("click", () =>
+  closeModal(editProfileModal)
+);
+
+editProfileForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  closeModal(editProfileModal);
+});
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
-
-const profileNameEl = document.querySelector(".profile__name");
-const profileDescriptionEl = document.querySelector(".profile__description");
-
-editProfileBtn.addEventListener("click", function () {
-  editProfileNameInput.value = profileNameEl.textContent;
-  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_is-opened");
-});
-
-editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
-});
-
-newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
-});
-
-newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
-});
-
-function handleEditProfileSubmit(evt) {
-  evt.preventDefault();
-  profileNameEl.textContent = editProfileNameInput.value;
-  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
-}
-
-editProfileForm.addEventListener("submit", handleEditProfileSubmit);
-
-//New
-
-const addCardFormElement = newPostModal.querySelector(".modal__form");
-const nameInput = newPostModal.querySelector("#profile-caption-input");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const captionInput = newPostModal.querySelector("#profile-caption-input");
 const linkInput = newPostModal.querySelector("#card-image-input");
 
-function handleAddCardSubmit(evt) {
-  evt.preventDefault();
-  console.log("Caption:", nameInput.value);
-  console.log("Image link:", linkInput.value);
-  newPostModal.classList.remove("modal_is-opened");
-}
+newPostBtn.addEventListener("click", () => {
+  newPostForm.reset();
+  openModal(newPostModal);
+});
 
-addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+newPostCloseBtn.addEventListener("click", () => closeModal(newPostModal));
+
+newPostForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  console.log("Caption:", captionInput.value);
+  console.log("Image link:", linkInput.value);
+  closeModal(newPostModal);
+});
+
+[editProfileModal, newPostModal].forEach((modal) => {
+  modal.addEventListener("click", (evt) => {
+    if (evt.target === modal) closeModal(modal);
+  });
+});
+
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    [editProfileModal, newPostModal].forEach(closeModal);
+  }
+});
